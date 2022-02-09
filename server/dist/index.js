51ea538accf1b1afbd79b88c102edd66cf6d1ae0
neerajp99 <neerajp1999@gmail.com>
2022-02-09 13:06:30 +0100

    Update with tdd setup

--- server/dist/index.js
+++ server/dist/index.js
@@ -5,12 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-const port = 8080; // default port to listen
-// define a route handler for the default home page
+const port = 8080;
 app.get("/", (req, res) => {
-    res.send("Hello world!");
+    res.send("Digifundo!");
 });
-// start the Express server
 app.listen(port, () => {
     console.log(`server started at http://localhost:${port}`);
 });
---
+++ server/tests/index.ts
@@ -0,0 +1,11 @@
+import { expect } from "chai";
+import "mocha";
+
+// Test test
+describe("This", () => {
+  describe("should", () => {
+    it("always pass", () => {
+      expect(true).to.equal(true);
+    });
+  });
+});
