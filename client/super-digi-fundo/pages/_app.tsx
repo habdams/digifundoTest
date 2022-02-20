e97d468ce2cc421c0ee5b11536b4b14f1065a2a0
neerajp99 <neerajp1999@gmail.com>
2022-02-20 18:03:19 +0100

    Update app for store

--- client/super-digi-fundo/pages/_app.tsx
+++ client/super-digi-fundo/pages/_app.tsx
@@ -2,6 +2,8 @@ import type { ReactElement, ReactNode } from 'react'
 import type { NextPage } from 'next'
 import type { AppProps } from 'next/app'
 import '../styles/globals.css'
+import { Provider } from 'react-redux'
+import store from '../app/store'
 
 type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode
@@ -13,7 +15,13 @@ type AppPropsWithLayout = AppProps & {
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page)
-  return getLayout(<Component {...pageProps} />)
+  return (
+    getLayout(
+    <Provider store = {store}>
+      <Component {...pageProps} />
+    </Provider>
+    )
+  )
 }
 
 export default MyApp
