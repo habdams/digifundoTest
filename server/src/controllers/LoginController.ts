4459558eac8766e551d57746c30e6f5332e56d35
neerajp99 <neerajp1999@gmail.com>
2022-03-04 10:38:13 +0100

    Update login for account verification

--- server/src/controllers/LoginController.ts
+++ server/src/controllers/LoginController.ts
@@ -57,48 +57,56 @@ class LoginController {
                 });
                 // return res.redirect("/login");
             } else {
-                bcrypt.compare(password, user.password).then((isMatch) => {
-                    if (isMatch) {
-                        // Create a token
-                        const payload = {
-                            id: user.id,
-                            email: user.email,
-                        };
-                        const token = jwt.sign(
-                            payload,
-                            process.env.JWT_SECRET,
-                            {
-                                expiresIn: "2d",
-                            },
-                            (error, token) => {
-                                if (error) {
-                                    return next(error);
+                if (user.isVerified) {
+                    bcrypt.compare(password, user.password).then((isMatch) => {
+                        if (isMatch) {
+                            // Create a token
+                            const payload = {
+                                id: user.id,
+                                email: user.email,
+                            };
+                            const token = jwt.sign(
+                                payload,
+                                process.env.JWT_SECRET,
+                                {
+                                    expiresIn: "2d",
+                                },
+                                (error, token) => {
+                                    if (error) {
+                                        return next(error);
+                                    }
+                                    return res.json({
+                                        success: true,
+                                        token: "Bearer " + token,
+                                    });
                                 }
-                                return res.json({
-                                    success: true,
-                                    token: "Bearer " + token,
-                                });
-                            }
-                        );
+                            );
 
-                        // Set the cookie
-                        res.cookie("jwt", token, {
-                            httpOnly: true,
-                            maxAge: 1000 * 60 * 60 * 24 * 365,
-                        });
+                            // Set the cookie
+                            res.cookie("jwt", token, {
+                                httpOnly: true,
+                                maxAge: 1000 * 60 * 60 * 24 * 365,
+                            });
 
-                        // Redirect to the dashboard
-                        return res.status(200).json({
-                            success: true,
-                        });
-                    } else {
-                        errorsObject.password = "Incorrect password";
-                        return res.status(400).json({
-                            errors: errorsObject,
-                        });
-                        // return res.redirect("/login");
-                    }
-                });
+                            // Redirect to the dashboard
+                            return res.status(200).json({
+                                success: true,
+                            });
+                        } else {
+                            errorsObject.password = "Incorrect password";
+                            return res.status(400).json({
+                                errors: errorsObject,
+                            });
+                            // return res.redirect("/login");
+                        }
+                    });
+                } else {
+                    errorsObject.email = "Please verify your email first";
+                    return res.status(400).json({
+                        errors: errorsObject,
+                    });
+                    // return res.redirect("/login");
+                }
             }
         }
     );
