16c254e255744ff0a01c5861e2123e96aafb6618
neerajp99 <neerajp1999@gmail.com>
2022-03-08 18:08:32 +0100

    Call the functions to app

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,28 +1,28 @@
 import express, { Application, Request, Response } from "express";
 import helmet from "helmet";
-import * as bodyParser from 'body-parser';
-import * as kue from 'kue';
-import * as path from 'path';
-import * as dotenv from 'dotenv'
+import * as bodyParser from "body-parser";
+import * as kue from "kue";
+import * as path from "path";
+import * as dotenv from "dotenv";
 import Express from "./providers/Express";
 import ConfigEnvironment from "./providers/ConfigEnvironment";
-import {Log} from "./middlewares/Log";
+import { Log } from "./middlewares/Log";
+import { Database } from "./providers/Database";
+import Queue from "./providers/Queue";
 
 /**
  * Initialise the logger instance
  */
-const logger = Log.logInstance(
-    'info',
-    'App' 
-);
+const logger = Log.logInstance("info", "App");
 
-class App{
+class App {
     /**
      * Load the environment variables
      */
     public loadEnv(): void {
+        logger.info("Loading the environment variables...");
         dotenv.config({
-            path: path.join(__dirname, '../.env')
+            path: path.join(__dirname, "../.env"),
         });
     }
 
@@ -30,6 +30,7 @@ class App{
      * Load the express server
      */
     public loadExpress(): void {
+        logger.info("Loading the express server...");
         Express.init();
     }
 
@@ -38,29 +39,53 @@ class App{
      */
     public loadKue(): void {
         const queue = kue.createQueue({
-            prefix: 'q',
+            prefix: "q",
             redis: {
                 port: process.env.REDIS_PORT,
                 host: process.env.REDIS_HOST,
-                auth: process.env.REDIS_PASSWORD
-            }
+                auth: process.env.REDIS_PASSWORD,
+            },
         });
         queue.watchStuckJobs(1000);
     }
 
     /**
-     * Load the queue monitoring    
+     * Load the mongo database
+     */
+    public loadMongo(): void {
+        logger.info("Loading the mongo database");
+        console.log("Loading the mongo database");
+        Database.init();
+    }
+
+    /**
+     * Clear the queue console
+     */
+    public clearQueueConsole(): void {
+        process.stdout.write("\x1B[2J\x1B[0f");
+
+        Queue.dispatch(
+            "checkout",
+            { foo: "bar", fizz: "buzz" },
+            (_data: any): void => {
+                logger.info(`>>> Here goes the data: ${_data}`);
+            }
+        );
+    }
+
+    /**
+     * Load the queue monitoring
      */
     public loadQueueMonitoring(): void {
-        const checkForMonitoring: boolean = ConfigEnvironment.config().queueMonitor;
+        const checkForMonitoring: boolean =
+            ConfigEnvironment.config().queueMonitor;
         const queuePort: number = ConfigEnvironment.config().queuePort;
 
-        if(checkForMonitoring) {
-            console.log('Happy')
+        if (checkForMonitoring) {
             kue.app.listen(queuePort);
             logger.info("Queue monitoring started at port: " + queuePort);
         }
     }
 }
 
-export default new App; 
+export default new App();
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,23 +1,31 @@
-import * as _cluster from 'cluster'
-import { cpus } from 'os';
-import * as process from 'process';
-import App from "./app"
+import * as _cluster from "cluster";
+import { cpus } from "os";
+import * as process from "process";
+import App from "./app";
 
 const numCPUs = cpus().length;
 const cluster = _cluster as unknown as _cluster.Cluster;
 
-// If one of the cpu is a master 
+// If one of the cpu is a master
 if (cluster.isPrimary) {
-    const CPUs: any = cpus(); 
+    // Clear the console
+    App.clearQueueConsole();
+
+    // Load the configurations
+    App.loadEnv();
+
+    const CPUs: any = cpus();
     // For each CPU process, fork a new process
     CPUs.forEach(() => {
-        cluster.fork()
-    })  
+        cluster.fork();
+    });
 
     // Load the queue monitor for the app
     App.loadQueueMonitoring();
-
 } else {
+    // Load the database
+    App.loadMongo();
+
     // Load the Express server
     App.loadExpress();
 }
