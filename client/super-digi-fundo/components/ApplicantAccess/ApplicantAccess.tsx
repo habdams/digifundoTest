a849e6c024f055a1ff90bd001fe3d78e76d4c067
Adetunji Ojekunle <=>
2022-03-10 23:02:39 +0100

    access page style update

--- client/super-digi-fundo/components/ApplicantAccess/ApplicantAccess.tsx
+++ client/super-digi-fundo/components/ApplicantAccess/ApplicantAccess.tsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import Image from 'next/image';
+
+
+
 
 export function ApplicantAccess() {
 
@@ -6,8 +10,9 @@ export function ApplicantAccess() {
     return (
         <>
             <div className='flex flex-col justify-center items-center h-screen'>
+                <Image src={'/padlock.svg'} height={75} width={75}/>
                 <div className='flex flex-col items-center my-10' >
-                    <h1 className='text-sm p-2 rounded border border-none bg-pink-600 text-white mb-2'>Anwendungs-ID</h1>
+                    <h1 className='text-sm p-2 rounded border border-none bg-pink-100 text-pink-600 mb-2'>Anwendungs-ID</h1>
                     <p className='text-4xl font-mono mb-5 px-2'>68HTU789</p>
                     <p className='px-2 max-w-xl text-center text-gray-600'>Bitte geben Sie den an Ihre E-Mail gesendeten Zugangscode in das Feld unten ein.</p>
                 </div>
