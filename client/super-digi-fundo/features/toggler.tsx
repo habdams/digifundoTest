2bcbdd5e1c27bc49b25f727c07d8b2ba6a312cbc
Adetunji Ojekunle <=>
2022-03-19 17:42:12 +0100

    comment out obstructive unusedfeature

--- client/super-digi-fundo/features/toggler.tsx
+++ client/super-digi-fundo/features/toggler.tsx
@@ -1,25 +1,25 @@
-import {
-    createSlice,
-    PayloadAction,
-} from '@reduxjs/toolkit';
-import type { RootState } from '../app/store';
+// import {
+//     createSlice,
+//     PayloadAction,
+// } from '@reduxjs/toolkit';
+// import type { RootState } from '../app/store';
 
-interface ToggleState {
-    value: boolean;
-};
+// interface ToggleState {
+//     value: boolean;
+// };
 
-const initialState = { value: false } as ToggleState
+// const initialState = { value: false } as ToggleState
 
-const toggleSlice = createSlice({
-    name: "toggleSidebar",
-    initialState,
-    reducers: {
-        show(state){
-            state.value = 
-        },
-        hide(state){
+// const toggleSlice = createSlice({
+//     name: "toggleSidebar",
+//     initialState,
+//     reducers: {
+//         show(state){
+//             state.value = 
+//         },
+//         hide(state){
 
-        }
-    },
+//         }
+//     },
 
-})
+// })
